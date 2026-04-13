f00acf90d2c0780465a30032f16ac211c04b3a95
jshuao <ptjosh25@gmail.com>
2026-04-13 12:30:17 -0400

    Updated schema to have NOT NULL for row_num

--- schema.sql
+++ schema.sql
@@ -46,7 +46,7 @@ CREATE TABLE IF NOT EXISTS ride_entries (
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     phone TEXT NOT NULL,
     info TEXT,
-    row_num INTEGER DEFAULT 1,
+    row_num INTEGER NOT NULL DEFAULT 1,
 
     PRIMARY KEY (announcement_id, user_id)
 );
